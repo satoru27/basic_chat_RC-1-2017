4295a7e4ae0c87e320ed28959aa8df9e32b126ed
satoru <vitorsatoru@hotmail.com>
2017-06-10 12:17:19 -0300

    minor changes and too many arguments case added

--- main.c
+++ main.c
@@ -12,58 +12,61 @@ int main(int argc, char *argv[]){
   switch(argc){
 
     case 1:
-    printf("[!] More arguments are needed, exiting...\n");
-    return -1;
+      printf("[!] More arguments are needed, exiting...\n");
+      return -1;
 
     case 2:
-    if(strcmp(argv[1],"-S")!=0 && strcmp(argv[1],"host")!=0){
-      printf("[!] Invalid mode, exiting...\n");
-      return -1;
-    }
-      mode=argv[1];
-      break;
+      if(strcmp(argv[1],"-S")!=0 && strcmp(argv[1],"host")!=0){
+        printf("[!] Invalid mode, exiting...\n");
+        return -1;
+      }
+        mode=argv[1];
+        break;
 
     case 3:
-    if(strcmp(argv[1],"-S")!=0 && strcmp(argv[1],"host")!=0 ){
-      printf("[!] Invalid mode, exiting...\n");
-      return -1;
-    }
-    mode=argv[1];
-    temp = strtol(argv[2],NULL,0);
-    if(temp < 1024){
-      printf("[!] Invalid port, default port (33333) was selected\n");
-    }
-    else{
-    port=temp;
-    }
-    break;
+      if(strcmp(argv[1],"-S")!=0 && strcmp(argv[1],"host")!=0 ){
+        printf("[!] Invalid mode, exiting...\n");
+        return -1;
+      }
+      mode=argv[1];
+      temp = strtol(argv[2],NULL,0);
+      if(temp < 1025){
+        printf("[!] Invalid port, default port (33333) was selected\n");
+      }
+      else{
+      port=temp;
+      }
+      break;
 
-    case 4:
+    default:
+      if(argc > 4){
+        printf("[!] Too many arguments (max = 3), exceeding arguments were ignored\n");
+      }
+      if(strcmp(argv[1],"-S")!=0 && strcmp(argv[1],"host")!=0 ){
+        printf("[!] Invalid mode, exiting...\n");
+        return -1;
+      }
+      mode=argv[1];
 
-    if(strcmp(argv[1],"-S")!=0 && strcmp(argv[1],"host")!=0 ){
-      printf("[!] Invalid mode, exiting...\n");
-      return -1;
-    }
-    mode=argv[1];
+      temp = strtol(argv[2],NULL,0);
 
-    temp = strtol(argv[2],NULL,0);
+      if(temp < 1025){
+        printf("[!] Invalid port, default port (33333) was selected\n");
+      }
+      else{
+      port=temp;
+      }
 
-    if(temp < 1024){
-      printf("[!] Invalid port, default port (33333) was selected\n");
-    }
-    else{
-    port=temp;
-    }
-
-    if(strcmp(argv[3],"-t")!=0 && strcmp(argv[3],"-u")!=0){
-      printf("[!] Invalid selected protocol, using default protocol (UDP)\n");
-    }
-    else{
-      protocol = argv[3];
-    }
-    break;
+      if(strcmp(argv[3],"-t")!=0 && strcmp(argv[3],"-u")!=0){
+        printf("[!] Invalid selected protocol, using default protocol (UDP)\n");
+      }
+      else{
+        protocol = argv[3];
+      }
+      break;
 
     }
+
 
   if(strcmp(mode,"-S")==0){
     printf("[.] Operating mode: server\n");
