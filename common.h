0dbac48368faf2c0558b6d9dad74c89bd2a16598
satoru <vitorsatoru@hotmail.com>
2017-06-19 19:08:11 -0300

    removed unnecessary libs

--- common.h
+++ common.h
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+//#include <sys/socket.h>
+//#include <sys/types.h>
+//#include <netinet/in.h>
+//#include <arpa/inet.h>
 #include <time.h>
 #include <unistd.h>
 #include <netdb.h>
